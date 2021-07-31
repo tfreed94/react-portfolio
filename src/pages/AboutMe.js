3124406d77f6faa6b890ee708ed92d16977591ac
Troy Freed <tfreed94@gmail.com>
2021-07-31 18:59:49 -0400

    fixed AboutMe, should be good for CSS.

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -4,10 +4,10 @@ import myPic from "";
 const AboutMe = () => {
     return (
         <div className="container card text-white bg-dark d-flex ">
-            <img src={myPic} className="img-fluid mx-auto rounded-circle" alt="profile" />
+            <img src={myPic} className="img-fluid mx-auto rounded-circle" alt="pic of me" />
             <div className="d-flex justify-content-center">
-                <h1 className="text-center mx-2 p-3">About Me</h1>
-                <p className="scroll mx-2 p-3 ">Future Full-Stack Web Developer with experience in web development tech like HTML, CSS, Bootstrap, JQuery, Javascript, and API's. My greatest strength in coding would be my ability to quickly and accurately fix errors and to break large projects into smaller more manageable pieces. I am a quick learner and passionate to display my coding skills in developing new and interesting websites</p>
+                <h1 className="text-center">About Me</h1>
+                <p className="scroll">Future Full-Stack Web Developer with experience in web development tech like HTML, CSS, Bootstrap, JQuery, Javascript, and API's. My greatest strength in coding would be my ability to quickly and accurately fix errors and to break large projects into smaller more manageable pieces. I am a quick learner and passionate to display my coding skills in developing new and interesting websites</p>
             </div>
         </div>
     )
