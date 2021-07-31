0a8d4774d81452a3e9844db37cd9e4cb1a5b4290
Troy Freed <tfreed94@gmail.com>
2021-07-31 12:19:30 -0400

    added header

---
+++ src/components/Header/header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+const Header = () => {
+    return (
+        <div>
+            <div className="jumbotron navbar-dark bg-dark text-white d-flex justify-content-between">
+                <h1 className="display-6">Troy Freed Jr</h1>
+                <p className="lead"> Welcome to my React-Portfolio! </p>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
