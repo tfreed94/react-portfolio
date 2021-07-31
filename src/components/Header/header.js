200f5e04b04737aa0a56a7bf8e894a0df95a6360
Troy Freed <tfreed94@gmail.com>
2021-07-31 19:53:45 -0400

    adjusted header

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -2,9 +2,8 @@ import React from 'react'
 const Header = () => {
     return (
         <div>
-            <div className="jumbotron navbar-dark bg-dark text-white d-flex justify-content-between">
+            <div className="jumbotron navbar-dark bg-dark text-white d-flex justify-content-center">
                 <h1 className="display-6">Troy Freed Jr</h1>
-                <p className="lead"> Welcome to my React-Portfolio! </p>
             </div>
         </div>
     )
