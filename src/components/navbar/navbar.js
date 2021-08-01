742527177ba33b43a8615c501f4b545c91919e61
Troy Freed <tfreed94@gmail.com>
2021-07-31 20:42:15 -0400

    adjusted content, css, and fixed issue with my resume link not working

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -2,20 +2,21 @@ import React from 'react';
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
+    const resURL = 'https://drive.google.com/file/d/1E9aUY1U31kgjP0oyKnx6u50lXRhYZUSi/view?usp=sharing'
   return (
     <div className='nav-container bg-warning pb-1 mb-1'>
       <nav className="navbar navbar-expand-lg navbar-toggleable-sm">
         <div className="container-fluid d-flex justify-content-center">
           <div className="list-container">
-            <ul className="navbar-nav">
+            <ul className="navbar-nav ">
               <li className="nav-item">
-                <Link className="link" to='/' >About Me</Link>
+                <Link className="link mx-2" to='/' >About Me</Link>
               </li>
               <li className="nav-item">
-                <Link className="link" to='/mywork' >My Work</Link>
+                <Link className="link mx-2" to='/mywork' >My Work</Link>
               </li>
               <li className="nav-item">
-                <Link className="link" to='https://drive.google.com/file/d/1E9aUY1U31kgjP0oyKnx6u50lXRhYZUSi/view?usp=sharing' >Resume</Link>
+                <Link className="link mx-2" to= {resURL} >Resume</Link>
               </li>
             </ul>
           </div>
