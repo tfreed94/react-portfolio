8706b5dd5fb91b74119667127d31d1711b325eeb
Troy Freed <tfreed94@gmail.com>
2021-07-31 20:00:55 -0400

    adjusted paragraph placement on page

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -6,8 +6,8 @@ const AboutMe = () => {
         <div className="container card text-white bg-dark d-flex ">
             <img src={myPic} className="img-fluid mx-auto rounded-circle" alt="Pic Of Me" />
             <div className="d-flex justify-content-center">
-                <h1 className="text-center">About Me</h1>
-                <p className="scroll">Future Full-Stack Web Developer with experience in web development tech like HTML, CSS, Bootstrap, JQuery, Javascript, and API's. My greatest strength in coding would be my ability to quickly and accurately fix errors and to break large projects into smaller more manageable pieces. I am a quick learner and passionate to display my coding skills in developing new and interesting websites</p>
+                <h1 className="text-center mx-4 p-4">About Me</h1>
+                <p className="scroll mx-4 p-5">Future Full-Stack Web Developer with experience in web development tech like HTML, CSS, Bootstrap, JQuery, Javascript, and API's. My greatest strength in coding would be my ability to quickly and accurately fix errors and to break large projects into smaller more manageable pieces. I am a quick learner and passionate to display my coding skills in developing new and interesting websites</p>
             </div>
         </div>
     )
